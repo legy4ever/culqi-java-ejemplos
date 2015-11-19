002a2e145ac5a598913974d56db3fb9fb7a2f7d9
William Muro <william.muro@culqi.com>
2015-11-19 12:47:21 -0500

    Update CrearVentaTest.java

--- Ejemplo(Spring)/src/test/java/com/culqi/pruebas/CrearVentaTest.java
+++ Ejemplo(Spring)/src/test/java/com/culqi/pruebas/CrearVentaTest.java
@@ -34,8 +34,8 @@ public class CrearVentaTest {
     @Test
     public void crearVentaTest() {
 
-        Culqi.llaveSecreta = "demo";
-        Culqi.codigoComercio = "JlhLlpOB5s1aS6upiioJkmdQ0OYZ6HLS2+/o4iYO2MQ=";
+        Culqi.codigoComercio = "demo";
+        Culqi.llaveSecreta = "JlhLlpOB5s1aS6upiioJkmdQ0OYZ6HLS2+/o4iYO2MQ=";
         Culqi.servidorBase = "https://integ-pago.culqi.com";
 
         String orden = randomOrder();
